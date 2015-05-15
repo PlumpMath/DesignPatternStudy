89397cd4fb17a6718f405b844e1b14887fca1e85
liupanpan <liupanpan@oneapm.com>
2015-05-15 19:00:59 +0800

    add singleton design pattern

--- AbstractFactory/Program.cs
+++ AbstractFactory/Program.cs
@@ -9,7 +9,9 @@ using AbstractFactory.Factory;
 using AbstractFactory.Product;
 /*
  *目的: 抽象工厂模式
- *理解：每个抽象工厂可以生产一个产品族,
+ *理解：每个抽象工厂可以生产一个产品族,我认为抽象工厂是工厂方法的扩展，虽然在添加一个产品，不需要重新新建一个工厂，但是会破坏已有结构，必须修改接口，然后再工厂中添加生产方法
+ *
+ *区别：工厂方法是产品与工厂 是一一对应的关系，但是抽象工厂中 工厂与产品是一对多的关系（每个工厂生产一个产品族）。
  * 
  */
 
@@ -20,6 +22,7 @@ namespace AbstractFactory
     {
         static void Main(string[] args)
         {
+            
             //生产Benz
             IFactory benzFactory = new BenzFactory();
             
---
+++ Singleton/Agent.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public class Agent:IAgent
+    {
+        private static readonly AgentSingleton singleton = new AgentSingleton();
+
+        public static IAgent Instance
+        {
+            get
+            {
+                try
+                {
+                    return singleton.ExistingInstance;
+                }
+                catch (NullReferenceException)
+                {
+                    return null;
+                }
+            }
+        }
+        private sealed class AgentSingleton : Singleton<IAgent>
+        {
+            public AgentSingleton()
+                : base(null)
+            {
+            }
+
+            protected override IAgent CreateInstance()
+            {
+                 Agent agent = new Agent();
+                 return agent;  
+            }
+
+         
+        }
+    }
+}
